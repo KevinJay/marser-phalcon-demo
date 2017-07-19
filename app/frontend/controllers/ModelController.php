9748475e51a65698e3642e5a57f3826bbf9ef8cc
root <root@localhost.localdomain>
2017-07-19 20:01:31 +0800

    Phalcon入门教程之模型CURD(1)

--- app/frontend/controllers/ModelController.php
+++ app/frontend/controllers/ModelController.php
@@ -22,4 +22,171 @@ class ModelController extends \Phalcon\Mvc\Controller{
             print_r($e);
         }
     }
+
+    public function selectAction()
+    {
+        try {
+            $articleModel = new ArticlesModel();
+            //查询所有记录
+            $result = $articleModel->find();
+            //循环输出结果集
+            foreach ($result as $record) {
+                //  var_dump($record->aid);
+                //  var_dump($record->title);
+            }
+            //var_dump($result->toArray());
+            //var_dump(count($result));
+
+            //$result1 = $articleModel->findFirst(1);
+            //print_r($result1->toArray());
+            //print_r($result1);
+
+//            $result2 = $articleModel->find([
+//                'conditions' => 'aid = :aid: AND status = :status:',
+//                'bind' => [
+//                    'aid' => 2,
+//                    'status' => 1,
+//                ],
+//            ]);
+//            print_r($result2->toArray());
+
+//            $articleModel->find([
+//                'columns' => 'aid, title', //查询字段
+//                'conditions' => 'aid = :aid:',  //查询条件
+//                'bind' => [ //参数绑定
+//                    'aid' => 2
+//                ],
+//                'order' => 'aid DESC', //排序
+//                'limit' => 10, //限制查询结果的数量
+//                'offset' => 10, //偏移量
+//            ]);
+
+//            $result3 = $articleModel->find([
+//                'conditions' => 'aid IN ({aids:array})',
+//                'bind' => [
+//                    'aids' => [1, 2]
+//                ],
+//            ]);
+//            print_r($result3->toArray());
+
+            $result4 = $articleModel->find([
+                'conditions' => 'title like :title:',
+                'bind' => [
+                    'title' => '%英语%',
+                ],
+            ]);
+            print_r($result4->toArray());
+
+        } catch (\Exception $e) {
+            echo '<pre>';
+            print_r($e);
+        }
+    }
+
+    public function insertAction(){
+        $articleModel = new ArticlesModel();
+        $result = $articleModel->create([
+            'title' => 'phalcon测试',
+            'introduce' => 'Phalcon入门教程',
+            'status' => 1,
+            'view_number' => 1,
+            'is_recommend' => 1,
+            'is_top' => 1,
+            'create_by' => 1,
+            'create_time' => date('Y-m-d H:i:s'),
+            'modify_by' => 1,
+            'modify_time' => date('Y-m-d H:i:s')
+        ]);
+        if (!$result) {
+            $errorMessage = implode(',', $articleModel->getMessages());
+            echo $errorMessage;
+        }else {
+            $aid = $articleModel->aid;
+            echo $aid;
+        }
+    }
+
+    public function batchInsertAction(){
+//        $articleModel = new ArticlesModel();
+//        //var_dump($articleModel->title);
+//        for ($i = 1; $i <= 10; $i++) {
+//            $data = [
+//                'title' => "phalcon测试{$i}",
+//                'introduce' => "Phalcon入门教程{$i}",
+//                'status' => $i,
+//                'view_number' => $i,
+//                'is_recommend' => 1,
+//                'is_top' => 1,
+//                'create_by' => $i,
+//                'create_time' => date('Y-m-d H:i:s'),
+//                'modify_by' => $i,
+//                'modify_time' => date('Y-m-d H:i:s')
+//            ];
+//            $result = $articleModel->create($data);
+//            if (!$result) {
+//                $errorMessage = implode(',', $articleModel->getMessages());
+//                exit($errorMessage);
+//            }else {
+//                $aid = $articleModel->aid;
+//                echo $aid;
+//                //var_dump($articleModel->title);
+//            }
+//            echo '<br />';
+//        }
+
+//        $articleModel = new ArticlesModel();
+//        for ($i = 1; $i <= 10; $i++) {
+//            $data = [
+//                'title' => "phalcon测试{$i}",
+//                'introduce' => "Phalcon入门教程{$i}",
+//                'status' => $i,
+//                'view_number' => $i,
+//                'is_recommend' => 1,
+//                'is_top' => 1,
+//                'create_by' => $i,
+//                'create_time' => date('Y-m-d H:i:s'),
+//                'modify_by' => $i,
+//                'modify_time' => date('Y-m-d H:i:s')
+//            ];
+//            $clone = clone $articleModel;
+//            $result = $clone->create($data);
+//            if (!$result) {
+//                $errorMessage = implode(',', $clone->getMessages());
+//                exit($errorMessage);
+//            }else {
+//                $aid = $clone->aid;
+//                echo $aid;
+//            }
+//            echo '<br />';
+//        }
+
+        $articleModel = new ArticlesModel();
+        $result = $articleModel->batch_insert([
+            [
+                'title' => "phalcon测试1",
+                'introduce' => "Phalcon入门教程1",
+                'status' => 1,
+                'view_number' => 1,
+                'is_recommend' => 1,
+                'is_top' => 1,
+                'create_by' => 1,
+                'create_time' => date('Y-m-d H:i:s'),
+                'modify_by' => 1,
+                'modify_time' => date('Y-m-d H:i:s')
+            ],
+            [
+                'title' => "phalcon测试2",
+                'introduce' => "Phalcon入门教程2",
+                'status' => 2,
+                'view_number' => 2,
+                'is_recommend' => 1,
+                'is_top' => 1,
+                'create_by' => 1,
+                'create_time' => date('Y-m-d H:i:s'),
+                'modify_by' => 1,
+                'modify_time' => date('Y-m-d H:i:s')
+            ],
+        ]);
+        var_dump($result);
+    }
 }
--- app/frontend/models/ArticlesModel.php
+++ app/frontend/models/ArticlesModel.php
@@ -18,4 +18,35 @@ class ArticlesModel extends \Marser\App\Frontend\Models\BaseModel {
         parent::initialize();
         $this->set_table_source(self::TABLE_NAME);
     }
+
+    /**
+     * 批量添加
+     * @param array $data
+     * @return boolean
+     * @throws \Exception
+     */
+    public function batch_insert(array $data){
+        if (count($data) == 0) {
+            throw new \Exception('参数错误');
+        }
+        $keys = array_keys(reset($data));
+        $keys = array_map(function ($key) {
+            return "`{$key}`";
+        }, $keys);
+        $keys = implode(',', $keys);
+        $sql = "INSERT INTO " . $this->getSource() . " ({$keys}) VALUES ";
+        foreach ($data as $v) {
+            $v = array_map(function ($value) {
+                return "'{$value}'";
+            }, $v);
+            $values = implode(',', array_values($v));
+            $sql .= " ({$values}), ";
+        }
+        $sql = rtrim(trim($sql), ',');
+        $result = $this->getDI()->get('db')->execute($sql);
+        if (!$result) {
+            throw new \Exception('批量入库记录');
+        }
+        return $result;
+    }
 }
